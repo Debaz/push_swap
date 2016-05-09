0f1f4e3f8905d98621b75adb8aad294256ecf3be
Kantin LESCAUDRON <klescaud@e1r2p23.42.fr>
2016-05-09 14:23:46 +0200

    parser 1.0

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: klescaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 10:55:13 by klescaud          #+#    #+#             */
-/*   Updated: 2016/05/09 10:56:04 by klescaud         ###   ########.fr       */
+/*   Updated: 2016/05/09 13:59:47 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define PUSH_SWAP_H
 
 # include <libft.h>
+
+/*
+** -----------------------------------------------------------------------------
+** array parsing functions (array_op.c):
+*/
+
+int		*add_value(int *array, int value);
+int		*copy_and_grow(int *array, int value);
+int		check_double(int *array, int value);
+void	print_int_tab(int *array);
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: klescaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/25 10:07:22 by klescaud          #+#    #+#             */
-/*   Updated: 2016/05/09 10:57:50 by klescaud         ###   ########.fr       */
+/*   Updated: 2016/05/09 14:17:05 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,29 @@
 
 int		main(int ac, char **av)
 {
+	int		*array;
+	int		i;
+
+	i = 1;
+	array = NULL;
 	if (ac > 1)
-		av[1] = NULL;
+		while (i < ac)
+		{
+			if (!check_double(array, ft_atoi(av[i])))
+			{
+				array = add_value(array, ft_atoi(av[i]));
+//				ft_putnbr(array[0]);
+//				ft_putchar(' ');
+				i++;
+			}
+			else
+			{
+				ft_putendl("Error");
+				free(array);
+				array = NULL;
+				break;
+			}
+		}
+	print_int_tab(array);
 	return (0);
 }
